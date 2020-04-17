80a5a21929dc55c710176cb79c11f25f93359dec
Biswas Angkan <48616181+BiswasAngkan@users.noreply.github.com>
2020-04-17 12:10:36 +0600

    Add files via upload

---
+++ Continuous_FaceHide.py
@@ -0,0 +1,26 @@
+# Angkan Biswas
+# 17.04.2020
+# To hide face in a taken picture.
+# Note: 1. Download 'haarcascade_frontalface_default.xml'
+# $ wget https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+# 2. Install 'opencv-contrib-python'
+# $ pip install opencv-contrib-python
+
+import cv2
+
+# Load pre-trained model
+model = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') 
+
+camera = cv2.VideoCapture(0)
+for i in range(200):
+	_, picture = camera.read()							# Take a picture
+
+	faces = model.detectMultiScale(picture)
+
+	for x, y, w, h  in faces:
+		cv2.rectangle(picture, (x, y), (x + w, y + h), (0, 0, 255), -1)
+
+	cv2.imshow("MyFace", picture)	
+	cv2.waitKey(1)									# Hold display window for a while
+
+camera.release()
