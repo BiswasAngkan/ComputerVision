134c9f574d8859bbe63748b9a9b05461ea8e854f
Biswas Angkan <48616181+BiswasAngkan@users.noreply.github.com>
2020-04-25 23:37:13 +0600

    Add files via upload

---
+++ Classify_By_VGG16.py
@@ -0,0 +1,42 @@
+#	Angkan Biswas
+#	25.04.2020
+#	--------------------------------------------
+#	Guess about the class (objects' name) of 
+#	an image using pre-trained model VGG16.
+#	-------------------------------------------
+
+'''	Load necessary modules.	'''
+from tensorflow.keras.applications.vgg16 import VGG16, decode_predictions
+from tensorflow.keras.preprocessing.image import load_img
+import cv2
+import numpy as np
+
+'''	Load the pre-trained model, VGG16.	'''
+model = VGG16()
+model.summary()
+
+'''	Load image.	'''
+imgPath = '/home/dell/downloads/Temple/10.Hindu-temple-1.jpg'
+img = load_img(imgPath, target_size = (224, 224))
+
+'''	Turn loaded image into a numpy array.	'''
+rgbImg = np.array(img, dtype = np.uint8)
+
+'''	Turn 3D image matrix into 4D matrix so that it can be accepted by VGG16.'''
+img = np.reshape(rgbImg, (1, 224, 224, 3))	
+
+'''	Guess about the object's name which occupies the image most.'''
+guess = model.predict(img)
+classLabel = decode_predictions(guess)
+print(classLabel)
+classLabel = classLabel[0][0][1]
+
+'''	TensorFlow loads image into RGB format while OpenCV looks image in
+	BGR format. So, we need to turn RGB image into BGR image, so that
+	we can use cv2.imshow().
+'''
+bgrImg = cv2.cvtColor(rgbImg, cv2.COLOR_RGB2BGR)	
+
+'''	Display the image using the OpenCV.	'''
+cv2.imshow(classLabel, bgrImg)
+cv2.waitKey(0)
